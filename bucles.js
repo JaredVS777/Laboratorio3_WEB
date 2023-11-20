876ea0bd34ff1b720e4e7c044d76f6433007687e
Jared Valenzuela <jared.valenzuela@epn.edu.ec>
2023-11-20 10:07:27 -0500

    Ejemplos de de Condicionales y Bucles

---
+++ bucles.js
@@ -0,0 +1,8 @@
+//Tabla de multipicar del 5
+let numero = 5;
+console.log("Tabla de Multiplicar del 5:");
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(numero + " x " + i + " = " + resultado);
+}
---
+++ conditions.js
@@ -0,0 +1,9 @@
+//Acceso a una pelicula de mayores de 18 años
+let edadUsuario = 20;
+let edadMinima = 18;
+
+if (edadUsuario >= edadMinima) {
+    console.log("¡Bienvenido! Puedes acceder a la película de 50 Sombras de Grey");
+} else {
+    console.log("Lo siento, eres demasiado joven para acceder a esta película.");
+}
